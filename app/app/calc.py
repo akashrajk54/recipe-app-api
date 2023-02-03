220b920f11d2e9a9729bd26729e62d5aea51be8f
Akash Bhandari <akash.bhandari@nishainfotech.com>
2023-02-03 11:59:22 +0530

    Fixed > test > missing comma

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,11 +3,11 @@ Calculator functions
 """
 
 
-def add(x,y):
+def add(x, y):
     """Add x and y and return result."""
     return x + y
 
 
-def subtract(x,y):
+def subtract(x, y):
     """subtract x and y and return result."""
     return y - x
