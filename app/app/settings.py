ae24fe814e6cd53816c2f1d6799a87bf6b4b2b11
Akash Bhandari <akash.bhandari@nishainfotech.com>
2023-02-10 09:53:07 +0530

    Deployment run successfully on local.

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,13 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v_x9=ibr%ad&8vpsmm2mk%#eoklx4^ux@1y*uv&bfgl^fk-o%e'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
